c09fede6ee9870b140366e491e61197c2a3fbf2a
Tse Kit Yam <tsekityam@users.noreply.github.com>
2021-07-10 22:57:52 +0800

    Implement parse .xlsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@material-ui/core";
+import XLSX from "xlsx";
 
 function App() {
   return (
@@ -8,16 +10,35 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Upload .XLSX that contains employee information
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button color="primary" variant="contained" component="label">
+          Upload File
+          <input
+            hidden
+            type="file"
+            // accept="application/vnd.ms-excel"
+            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              if (event.target.files) {
+                const reader = new FileReader();
+                reader.readAsBinaryString(event.target.files[0]);
+                reader.onload = function (e) {
+                  const data = e.target?.result;
+                  const workbook = XLSX.read(data, {
+                    type: "binary",
+                  });
+                  workbook.SheetNames.forEach(function (sheetName) {
+                    var result = XLSX.utils.sheet_to_json(
+                      workbook.Sheets[sheetName]
+                    );
+                    console.log(result);
+                  });
+                };
+              }
+            }}
+          />
+        </Button>
       </header>
     </div>
   );
